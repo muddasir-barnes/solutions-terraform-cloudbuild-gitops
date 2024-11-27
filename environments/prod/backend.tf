3acbe4199251726c8579267029422ea5bdfe082f
Muddasir Shakil <mshakil@onebarnes.com>
2024-11-27 15:44:18 +0000

    update project IDs and buckets
    Signed-off-by: Muddasir Shakil <mshakil@onebarnes.com>

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "devproject-442513-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="devproject-442513"
